9300ca728ac5600af732d3d150a52b191561c395
vvf11 <80396600+vvf11@users.noreply.github.com>
2025-03-01 22:10:56 +0300

    Add files via upload

---
+++ pkg/tetris_zkp_bg.wasm.d.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const malloc: (a: number) => number;
+export const free: (a: number, b: number) => void;
+export const generate_proof: (a: number, b: number) => number;
+export const __wbindgen_export_0: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_start: () => void;
